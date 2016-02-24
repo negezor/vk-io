eb7f3ec942f2f42e46702b66bc99e3889b12bf56
Vladlen <ultrashock98@gmail.com>
2016-02-24 19:19:49 +1000

    Move status.js object

--- index.js
+++ index.js
@@ -7,6 +7,42 @@ fs = require('fs'),
 util = require('util'),
 /* Основа модуля */
 io = function(){
+	/* Текущий статус модуля */
+	this.status = {
+		/* Кол-во ошибок за инициализацию */
+		error: 0,
+		/* Кол-во выполненых методов vk */
+		execute: 0,
+		/* Кол-во отправленных сообщений */
+		send: 0,
+		/* Кол-во полученных сообщений longpoll */
+		accepted: 0,
+		/* Задания */
+		tasks: {
+			/* Включен ли работник */
+			launched: false,
+			/* Список задач */
+			queue: [],
+			/* Идентификатор таймера */
+			id: null
+		},
+		/* Статус longpoll */
+		longpoll: {
+			/* Статус longpoll */
+			launched: false,
+			/* Пропуск ID сообщений */
+			skip: [],
+			/* Сервер longpoll */
+			server: null,
+			/* Ключ авторизации */
+			key: null,
+			/* TimeStamp последних событий */
+			ts: null,
+			/* Режим работы long poll */
+			mode: null
+		}
+	};
+
 	/* Список методов api */
 	this.api = {};
 	/* Установка базовых методов vk api */
