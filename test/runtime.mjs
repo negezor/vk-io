3963f862f261a324842363d36bc78c83b4975e16
Negezor <negezor@gmail.com>
2018-04-20 17:06:45 +1100

    Using current node for building

--- test/runtime.mjs
+++ test/runtime.mjs
@@ -1,1 +1,14 @@
-require('@babel/register')({});
+require('@babel/register')({
+	presets: [
+		['@babel/env', {
+			targets: {
+				node: 'current'
+			},
+			modules: 'cjs',
+			useBuiltIns: 'usage'
+		}]
+	],
+	plugins: [
+		'@babel/proposal-object-rest-spread'
+	]
+});
