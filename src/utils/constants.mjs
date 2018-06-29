c8fe36fd276e7df4120267eb2c82fc23c3c67c07
Negezor <negezor@gmail.com>
2018-06-29 19:55:49 +1100

    chore: Updated Desktop User-Agent

--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -35,7 +35,7 @@ export const CALLBACK_BLANK = 'https://oauth.vk.com/blank.html';
  *
  * @type {string}
  */
-export const DESKTOP_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36';
+export const DESKTOP_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36';
 
 /**
  * Minimum time interval api with error
