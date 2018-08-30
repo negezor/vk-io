4edf89c80d45ee19b567029c8e932a69bed642ae
Negezor <negezor@gmail.com>
2018-08-30 21:08:12 +1100

    feat: Improved link attachment

--- src/structures/attachments/link.mjs
+++ src/structures/attachments/link.mjs
@@ -1,5 +1,6 @@
 import ExternalAttachment from './external';
 
+import PhotoAttachment from './photo';
 import { attachmentTypes } from '../../utils/constants';
 
 const { LINK } = attachmentTypes;
@@ -15,6 +16,19 @@ export default class LinkAttachment extends ExternalAttachment {
 		super(LINK, payload);
 
 		this.vk = vk;
+
+		this.attachments = payload.photo
+			? [new PhotoAttachment(payload.photo, vk)]
+			: [];
+	}
+
+	/**
+	 * Checks for the presence of a photo in a link
+	 *
+	 * @return {boolean}
+	 */
+	get hasPhoto() {
+		return this.attachments.length > 0;
 	}
 
 	/**
@@ -36,11 +50,47 @@ export default class LinkAttachment extends ExternalAttachment {
 	}
 
 	/**
+	 * Returns the title
+	 *
+	 * @return {?string}
+	 */
+	get caption() {
+		return this.payload.caption || null;
+	}
+
+	/**
 	 * Returns the description
 	 *
-	 * @return {string}
+	 * @return {?string}
 	 */
 	get description() {
-		return this.payload.description;
+		return this.payload.description || null;
+	}
+
+	/**
+	 * Returns the photo
+	 *
+	 * @return {?PhotoAttachment}
+	 */
+	get photo() {
+		return this.attachments[0] || null;
+	}
+
+	/**
+	 * Returns the product
+	 *
+	 * @return {?Object}
+	 */
+	get product() {
+		return this.payload.product;
+	}
+
+	/**
+	 * Returns the button
+	 *
+	 * @return {?Object}
+	 */
+	get button() {
+		return this.payload.button || null;
 	}
 }
