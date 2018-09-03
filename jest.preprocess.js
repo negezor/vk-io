dc3203de2a5125858a5451d3f8d0be7a734ad210
Negezor <negezor@gmail.com>
2018-09-03 16:59:51 +1100

    misc: Remove jest preprocessor

--- jest.preprocess.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const babelJest = require('babel-jest');
-
-const babelConfig = require('./babel.config.js');
-
-module.exports = babelJest.createTransformer(babelConfig);
