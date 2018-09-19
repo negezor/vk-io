00c4d973889b66eda35fbafad8061e331d92b55e
Negezor <negezor@gmail.com>
2018-09-19 17:22:40 +1100

    test: Better tests for attachment

--- test/attachments.test.js
+++ test/attachments.test.js
@@ -23,6 +23,57 @@ describe('Attachments', () => {
 		expect(String(attachment)).toBe('photo1234_5678_ACCESS_KEY');
 	});
 
+	describe('the #fromString() should be correct working', () => {
+		it('should be throw an exception if wrong', () => {
+			expect(() => Attachment.fromString('ascbas_baasd')).toThrow();
+			expect(() => Attachment.fromString('12345/@%$%')).toThrow();
+			expect(() => Attachment.fromString('Incorrect')).toThrow();
+			expect(() => Attachment.fromString('1234_')).toThrow();
+		});
+
+		it('should be correct parse', () => {
+			expect(Attachment.fromString('photo1234_5678')).toMatchObject({
+				type: 'photo',
+				ownerId: 1234,
+				id: 5678,
+				accessKey: null
+			});
+
+			expect(Attachment.fromString('photo1234_5678_ACCESS_KEY')).toMatchObject({
+				type: 'photo',
+				ownerId: 1234,
+				id: 5678,
+				accessKey: 'ACCESS_KEY'
+			});
+		});
+	});
+
+	describe('the #equals() should be correct working', () => {
+		it('should be throw an exception if wrong', () => {
+			const attachment = new Attachment('photo', 1234, 5678);
+
+			expect(() => attachment.equals('ascbas_baasd')).toThrow();
+			expect(() => attachment.equals('inccorect')).toThrow();
+			expect(() => attachment.equals('1234_')).toThrow();
+		});
+
+		it('should be return false', () => {
+			const attachment = new Attachment('photo', 1234, 5678);
+
+			expect(attachment.equals('photo1234_1234')).toBe(false);
+			expect(attachment.equals(new Attachment('photo', 1234, 1234))).toBe(false);
+			expect(attachment.equals(new Attachment('photo', 1234, 1234, 'ACCESS_KEY'))).toBe(false);
+		});
+
+		it('should be return true', () => {
+			const attachment = new Attachment('photo', 1234, 5678);
+
+			expect(attachment.equals('photo1234_5678')).toBe(true);
+			expect(attachment.equals(new Attachment('photo', 1234, 5678))).toBe(true);
+			expect(attachment.equals(new Attachment('photo', 1234, 5678, 'ACCESS_KEY'))).toBe(true);
+		});
+	});
+
 	describe('should equivalent to attaching to a string', () => {
 		it('wall', () => {
 			const attachment = new WallAttachment({
