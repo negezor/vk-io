b9f909a6f491b9e40d9aaf64dc9253a2daf8e07a
Negezor <negezor@gmail.com>
2018-10-16 15:27:13 +1100

    fix: Added comments to collection methods

--- src/structures/shared/message-forwards-collection.mjs
+++ src/structures/shared/message-forwards-collection.mjs
@@ -14,6 +14,11 @@ const getForwards = (rootForwards) => {
 const kFlatten = Symbol('flatten');
 
 export default class MessageForwardsCollection extends Array {
+	/**
+	 * Returns a flat copy of forwards
+	 *
+	 * @return {MessageForward[]}
+	 */
 	get flatten() {
 		if (!this[kFlatten]) {
 			this[kFlatten] = getForwards(this);
@@ -22,12 +27,26 @@ export default class MessageForwardsCollection extends Array {
 		return this[kFlatten];
 	}
 
+	/**
+	 * Checks for the presence of attachments
+	 *
+	 * @param {?string} type
+	 *
+	 * @return {boolean}
+	 */
 	hasAttachments(type) {
 		return this.flatten.some(forward => (
 			forward.hasAttachments(type)
 		));
 	}
 
+	/**
+	 * Returns the attachments
+	 *
+	 * @param {?string} type
+	 *
+	 * @return {Array}
+	 */
 	getAttachments(type) {
 		const attachments = this.flatten.map(forward => (
 			forward.getAttachments(type)
