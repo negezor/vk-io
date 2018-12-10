804784145b8fa5615715521590bd59454ef2589b
Negezor <negezor@gmail.com>
2018-12-11 01:08:39 +1100

    misc: Update User-Agent

--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -28,7 +28,7 @@ export const CALLBACK_BLANK = 'https://oauth.vk.com/blank.html';
  *
  * @type {string}
  */
-export const DESKTOP_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36';
+export const DESKTOP_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36';
 
 /**
  * Minimum time interval api with error
