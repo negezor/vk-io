aabd70e105a1350f5fa3d687f2e63257d3f94643
Negezor <negezor@gmail.com>
2019-05-04 21:18:25 +1100

    misc(scenes): Use vk-io context

--- packages/session/src/types.ts
+++ packages/session/src/types.ts
@@ -1,8 +1,10 @@
+import { Context } from 'vk-io';
+
 export type Partial = {
 	[key: string]: any;
 };
 
-export interface IContext extends Partial {}
+export interface IContext extends Context {}
 
 export interface ISessionContext extends Partial {
 	$forceUpdate(): Promise<boolean>;
