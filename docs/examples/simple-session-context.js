93943ac03c544d2f088e06491cc0c50a4e0a7454
Negezor <negezor@gmail.com>
2019-05-08 17:16:39 +1100

    example: Update session example

--- docs/examples/simple-session-context.js
+++ docs/examples/simple-session-context.js
@@ -1,41 +1,22 @@
 const { VK } = require('vk-io');
+const { SessionManager } = require('@vk-io/session');
 
 const vk = new VK({
 	token: process.env.TOKEN
 });
 
-const memoryStorage = new Map();
+const sessionManager = new SessionManager();
 
-// Simple session middleware
-vk.updates.on('message', async (context, next) => {
-	const { peerId } = context;
+vk.updates.on('message', sessionManager.middleware);
 
-	const session = memoryStorage.has(peerId)
-		? memoryStorage.get(peerId)
-		: {};
-
-	context.session = session;
-
-	await next();
-
-	memoryStorage.set(peerId, session);
-});
-
-// Simple counter in the session
-vk.updates.on('message', async (context, next) => {
+vk.updates.hear('/counter', async (context) => {
 	const { session } = context;
 
-	if (!('counter' in session)) {
+	if (!session.counter) {
 		session.counter = 0;
 	}
 
 	session.counter += 1;
-
-	await next();
-});
-
-vk.updates.hear('/counter', async (context) => {
-	const { session } = context;
 
 	await context.send(`You turned to the bot (${session.counter}) times`);
 });
