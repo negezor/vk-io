12571b76237f77627df30f93a93393fa0163f11d
Negezor <negezor@gmail.com>
2019-06-07 21:18:49 +1100

    misc: Updated User-Agent for auth

--- packages/vk-io/src/utils/constants.js
+++ packages/vk-io/src/utils/constants.js
@@ -28,7 +28,7 @@ export const CALLBACK_BLANK = 'https://oauth.vk.com/blank.html';
  *
  * @type {string}
  */
-export const DESKTOP_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36';
+export const DESKTOP_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36';
 
 /**
  * Minimum time interval api with error
