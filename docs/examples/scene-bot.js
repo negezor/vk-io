219fa0c188c5b7c89d4d2cd54f2cbbb5b4a86051
Vladlen <negezor@gmail.com>
2019-08-24 18:52:42 +1100

    fix(examples): Add missing middlewareIntercept

--- docs/examples/scene-bot.js
+++ docs/examples/scene-bot.js
@@ -40,6 +40,7 @@ sceneManager.addScene(new StepScene('signup', [
 
 vk.updates.on('message', sessionManager.middleware);
 vk.updates.on('message', sceneManager.middleware);
+vk.updates.on('message', sceneManager.middlewareIntercept);
 
 vk.updates.hear('/signup', async (context) => {
 	await context.scene.enter('signup');
