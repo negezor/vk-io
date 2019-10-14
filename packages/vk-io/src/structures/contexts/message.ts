0d3cecfd27a38a525d964f90ba3941250b3eb431
Negezor <negezor@gmail.com>
2019-10-14 19:17:05 +1100

    fix(contexts): Add missed referral for inspect

--- packages/vk-io/src/structures/contexts/message.ts
+++ packages/vk-io/src/structures/contexts/message.ts
@@ -909,6 +909,10 @@ export default class MessageContext<S = Record<string, any>>
 
 		afterAttachments.push('isOutbox');
 
+		if (this.referralValue) {
+			afterAttachments.push('referralValue', 'referralSource');
+		}
+
 		if (this.$match) {
 			afterAttachments.push('$match');
 		}
