bced0ef68be2f21485fa75daa40f0be0403cf04d
Negezor <negezor@gmail.com>
2019-10-14 18:55:00 +1100

    misc(sessions): Delete has method from memory storage

--- packages/session/src/storages/memory.ts
+++ packages/session/src/storages/memory.ts
@@ -3,10 +3,6 @@ import ISessionStorage from './storage';
 export default class MemoryStorage implements ISessionStorage {
 	private store: Map<string, object> = new Map();
 
-	public async has(key: string): Promise<boolean> {
-		return this.store.has(key);
-	}
-
 	public async get(key: string): Promise<object> {
 		return this.store.get(key) || null;
 	}
