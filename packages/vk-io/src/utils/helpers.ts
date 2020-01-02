497d6da6f121fb5bb50e135bb7dc4f91c76af999
Negezor <negezor@gmail.com>
2020-01-03 00:06:01 +1100

    fix(types): strict set object type

--- packages/vk-io/src/utils/helpers.ts
+++ packages/vk-io/src/utils/helpers.ts
@@ -94,8 +94,7 @@ export const copyParams = <
 	T,
 	K extends keyof T
 >(params: T, properties: K[]): Pick<T, K> => {
-	// @ts-ignore
-	const copies: Pick<T, K> = {};
+	const copies: Pick<T, K> = {} as T;
 
 	for (const property of properties) {
 		copies[property] = params[property];
