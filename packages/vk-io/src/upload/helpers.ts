80fcc2954897a5090b1cd48161dc3b06bdf1d75c
Negezor <negezor@gmail.com>
2020-01-03 02:32:13 +1100

    fix(types): use pick instead of just generic

--- packages/vk-io/src/upload/helpers.ts
+++ packages/vk-io/src/upload/helpers.ts
@@ -14,7 +14,7 @@ export const copyParams = <
 	T,
 	K extends keyof T
 >(params: T, properties: K[]): Pick<T, K> => {
-	const copies: Pick<T, K> = {} as T;
+	const copies: Pick<T, K> = {} as Pick<T, K>;
 
 	for (const property of properties) {
 		if (params[property] !== undefined) {
--- packages/vk-io/src/utils/helpers.ts
+++ packages/vk-io/src/utils/helpers.ts
@@ -94,7 +94,7 @@ export const copyParams = <
 	T,
 	K extends keyof T
 >(params: T, properties: K[]): Pick<T, K> => {
-	const copies: Pick<T, K> = {} as T;
+	const copies: Pick<T, K> = {} as Pick<T, K>;
 
 	for (const property of properties) {
 		copies[property] = params[property];
