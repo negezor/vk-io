9b0446095b9fcadbc94b4ff9a33855dfa87baa09
Negezor <negezor@gmail.com>
2020-02-19 15:17:09 +1100

    BREAKING CHANGE(module): delete deprecated

--- packages/vk-io/src/vk.ts
+++ packages/vk-io/src/vk.ts
@@ -11,10 +11,6 @@ import { CallbackService } from './utils/callback-service';
 import { IVKOptions } from './types';
 
 import { defaultOptions } from './utils/constants';
-import { showDeprecatedMessage } from './utils/helpers';
-
-const kAuth = Symbol('auth');
-const kStreaming = Symbol('streaming');
 
 /**
  * Main class
@@ -42,12 +38,6 @@ export class VK {
 
 	public callbackService = new CallbackService(this);
 
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	protected [kAuth]: any;
-
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	protected [kStreaming]: any;
-
 	/**
 	 * Constructor
 	 */
@@ -60,30 +50,6 @@ export class VK {
 	 */
 	public get [Symbol.toStringTag](): string {
 		return this.constructor.name;
-	}
-
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	public get auth(): any {
-		if (!this[kAuth]) {
-			showDeprecatedMessage('vk.auth deprecated, use @vk-io/authorization instead');
-
-			// eslint-disable-next-line
-			this[kAuth] = new (require('@vk-io/authorization').Authorization)(this);
-		}
-
-		return this[kAuth];
-	}
-
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	public get streaming(): any {
-		if (!this[kStreaming]) {
-			showDeprecatedMessage('vk.streaming deprecated, use @vk-io/streaming instead');
-
-			// eslint-disable-next-line
-			this[kStreaming] = new (require('@vk-io/streaming').StreamingAPI)(this);
-		}
-
-		return this[kStreaming];
 	}
 
 	/**
