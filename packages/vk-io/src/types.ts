fab1e29a62b99738875f3012f791721176c17e1c
Negezor <negezor@gmail.com>
2020-03-30 17:17:04 +1100

    docs: add more info for vk options

--- packages/vk-io/src/types.ts
+++ packages/vk-io/src/types.ts
@@ -13,6 +13,8 @@ export interface IVKOptions {
 
 	/**
 	 * HTTPS agent
+	 *
+	 * @see {https://nodejs.org/api/https.html#https_class_https_agent}
 	 */
 	agent: Agent;
 
@@ -52,7 +54,9 @@ export interface IVKOptions {
 	authScope?: AllowArray<string> | number;
 
 	/**
-	 * Wait time for one auth request
+	 * Wait time in ms for one auth request
+	 *
+	 * @defaultValue `10000`
 	 */
 	authTimeout: number;
 
@@ -62,6 +66,8 @@ export interface IVKOptions {
 	 * - `parallel` - all requests are sent through execute
 	 * - `parallel_selected` - only the specified methods in `apiExecuteMethods`
 	 * are collected in `execute`, other methods as in `sequential` mode
+	 *
+	 * @defaultValue `sequential`
 	 */
 	apiMode: 'sequential' | 'parallel' | 'parallel_selected';
 
@@ -71,66 +77,92 @@ export interface IVKOptions {
 	 * - `sequential` - through the interval
 	 * - `burst` - in parallel,
 	 * the maximum number of requests (attention, may cause an EAI_AGAIN error)
+	 *
+	 * @defaultValue `sequential`
 	 */
 	apiRequestMode: 'sequential' | 'burst';
 
 	/**
 	 * Time to wait before re-querying
+	 *
+	 *  @defaultValue `3000`
 	 */
 	apiWait: number;
 
 	/**
 	 * Requests per second
+	 *
+	 * @defaultValue `3`
 	 */
 	apiLimit: number;
 
 	/**
 	 * VK API version
+	 *
+	 * @see {https://vk.com/dev/versions}
 	 */
 	apiVersion: string;
 
 	/**
 	 * Base API URL
+	 *
+	 * @defaultValue `https://api.vk.com/method`
 	 */
 	apiBaseUrl: string;
 
 	/**
 	 * The number of retries at calling
+	 *
+	 * @defaultValue `3`
 	 */
 	apiRetryLimit: number;
 
 	/**
 	 * Wait time for one request
+	 *
+	 * @defaultValue `10000`
 	 */
 	apiTimeout: number;
 
 	/**
 	 * Headers sent to the API
+	 *
+	 * @defaultValue `{ User-Agent': 'vk-io/${version} (+https://github.com/negezor/vk-io)' }`
 	 */
 	apiHeaders: Record<string, string>;
 
 	/**
 	 * Number of requests per execute
+	 *
+	 * @defaultValue `25`
 	 */
 	apiExecuteCount: number;
 
 	/**
 	 * Methods for call execute (apiMode=parallel_selected)
+	 *
+	 * @defaultValue `['messages.send']`
 	 */
 	apiExecuteMethods: string[];
 
 	/**
 	 * Wait time for one request
+	 *
+	 * @defaultValue `20000`
 	 */
 	uploadTimeout: number;
 
 	/**
 	 * Time to wait before re-querying
+	 *
+	 * @defaultValue `3000`
 	 */
 	pollingWait: number;
 
 	/**
 	 * The number of retries at calling
+	 *
+	 * @defaultValue `3`
 	 */
 	pollingRetryLimit: number;
 
@@ -151,6 +183,8 @@ export interface IVKOptions {
 
 	/**
 	 * The number of retries at calling
+	 *
+	 * @defaultValue `3`
 	 */
 	collectRetryLimit: number;
 }
