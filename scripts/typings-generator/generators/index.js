5fe6158413a30d50b1a524dbaad1ea7fde3e15e7
Negezor <negezor@gmail.com>
2020-07-22 03:32:16 +1100

    fix(scripts): delete export class generator

--- scripts/typings-generator/generators/index.js
+++ scripts/typings-generator/generators/index.js
@@ -1,9 +1,7 @@
 const TypesGenerator = require('./types');
-const ClassGenerator = require('./class');
 const InterfaceGenerator = require('./interface');
 
 module.exports = {
 	TypesGenerator,
-	ClassGenerator,
 	InterfaceGenerator
 };
