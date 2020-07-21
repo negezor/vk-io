1025b62ac01bd10d4a488019b7bc08920c8a1b09
Negezor <negezor@gmail.com>
2020-07-21 23:11:52 +1100

    WIP: add new test resource resolver

---
+++ packages/vk-io/src/utils/resource-resolver.ts
@@ -0,0 +1,149 @@
+import { API } from '../api';
+import { ResourceError } from '../errors';
+
+import { ResourceType, ResourceErrorCode } from './constants';
+
+export interface IResolveResourceOptions {
+	resource: string | number;
+	api?: API;
+}
+
+export interface IResolvedTargetResource {
+	id: number;
+	type: 'user' | 'group' | 'application' | 'albums' | 'videos' | 'audios';
+}
+
+export interface IResolvedOwnerResource {
+	id: number;
+	ownerId: number;
+	type: 'photo' | 'audio' | 'video' | 'doc' | 'wall' | 'topic';
+}
+
+const onlyNumberRe = /^-?d+$/;
+const systemMentionRe = /\[([^|]+)\|[^\]]+\]/;
+
+const isVKUrlRe = /^(?:https:\/\/)?(?:m\.)?vk.com\//;
+
+const parseTargetResourceRe = /^([^\d]+)(\d+)$/;
+const parseOwnerResourceRe = /([^\d]+)(\d+)_(\d+)/;
+
+const enumResourceTypes: Record<string, IResolvedTargetResource['type']> = {
+	id: ResourceType.USER,
+	club: ResourceType.GROUP,
+	public: ResourceType.GROUP,
+	app: ResourceType.APPLICATION
+};
+
+const transformNumberResourceToTarget = (resource: number): string => (
+	resource < 0
+		? `club${-resource}`
+		: `id${resource}`
+);
+
+const transformMentionResourceToTarget = (resource: string): string => {
+	const { 1: mentionResource } = resource.match(systemMentionRe)!;
+
+	return mentionResource;
+};
+
+const resolveTargetResouce = (resource: string): IResolvedTargetResource => {
+	const { 1: rawType, 2: rawId } = resource.match(parseTargetResourceRe)!;
+
+	return {
+		id: Number(rawId),
+		type: enumResourceTypes[rawType]
+	};
+};
+
+const resolveOwnerResource = (resource: string): IResolvedOwnerResource => {
+	const { 1: rawType, 2: rawOwnerId, 3: rawId } = resource.match(parseOwnerResourceRe)!;
+
+	return {
+		id: Number(rawId),
+		ownerId: Number(rawOwnerId),
+		type: rawType as IResolvedOwnerResource['type']
+	};
+};
+
+const resolveSlugResource = async (
+	resource: string,
+	api?: API
+): Promise<IResolvedTargetResource> => {
+	if (api === undefined) {
+		throw new Error('API object is not passed');
+	}
+
+	const response = await api.utils.resolveScreenName({
+		screen_name: resource
+	});
+
+	if (Array.isArray(response)) {
+		throw new ResourceError({
+			code: ResourceErrorCode.RESOURCE_NOT_FOUND,
+			message: 'Resource not found'
+		});
+	}
+
+	return {
+		id: response.object_id!,
+		type: response.type
+	};
+};
+
+export const resolveResource = async ({
+	resource: rawResource,
+	api
+}: IResolveResourceOptions): Promise<IResolvedTargetResource | IResolvedOwnerResource> => {
+	if (!rawResource) {
+		throw new TypeError('Resource is required');
+	}
+
+	const resource = String(rawResource).trim();
+
+	if (onlyNumberRe.test(resource)) {
+		return resolveTargetResouce(
+			transformNumberResourceToTarget(
+				Number(resource)
+			)
+		);
+	}
+
+	if (systemMentionRe.test(resource)) {
+		return resolveTargetResouce(
+			transformMentionResourceToTarget(
+				resource
+			)
+		);
+	}
+
+	if (!isVKUrlRe.test(resource)) {
+		return resolveSlugResource(resource, api);
+	}
+
+	const resourceUrl = !resource.startsWith('https://')
+		? `https://${resource}`
+		: resource;
+
+	const { pathname, search } = new URL(resourceUrl);
+
+	if (pathname === '/') {
+		throw new ResourceError({
+			code: ResourceErrorCode.INVALID_URL,
+			message: 'URL should contain path'
+		});
+	}
+
+	if (parseOwnerResourceRe.test(search)) {
+		return resolveOwnerResource(pathname);
+	}
+
+	if (parseOwnerResourceRe.test(pathname)) {
+		return resolveOwnerResource(pathname);
+	}
+
+	if (parseTargetResourceRe.test(pathname)) {
+		return resolveTargetResouce(pathname);
+	}
+
+	return resolveSlugResource(resource, api);
+};
