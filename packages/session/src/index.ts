3ee48b608c7fb4f2bfa01b905a11109d997023f9
Negezor <negezor@gmail.com>
2020-07-24 21:17:21 +1100

    BREAKING CHANGE(session): drop support default export

--- packages/session/src/index.ts
+++ packages/session/src/index.ts
@@ -2,5 +2,4 @@ export * from './storages';
 
 export * from './types';
 
-// eslint-disable-next-line import/no-default-export
-export { SessionManager, SessionManager as default } from './session-manager';
+export { SessionManager } from './session-manager';
