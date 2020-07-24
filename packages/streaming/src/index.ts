73454d2c0641ef038299892dc8ff7506ea7d7d41
Negezor <negezor@gmail.com>
2020-07-24 21:17:03 +1100

    BREAKING CHANGE(streaming): drop support default export

--- packages/streaming/src/index.ts
+++ packages/streaming/src/index.ts
@@ -2,4 +2,4 @@ export * from './errors';
 export * from './contexts';
 
 // eslint-disable-next-line import/no-default-export
-export { StreamingAPI, StreamingAPI as default } from './streaming';
+export { StreamingAPI } from './streaming';
