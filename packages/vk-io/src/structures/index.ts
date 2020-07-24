694e7f2e1a052e209b5218ffafc1089e812ad865
Negezor <negezor@gmail.com>
2020-07-24 21:13:53 +1100

    feat(structures): add index files

---
+++ packages/vk-io/src/structures/index.ts
@@ -0,0 +1,4 @@
+export * from './attachments';
+export * from './contexts';
+export * from './keyboard';
+export * from './shared';
---
+++ packages/vk-io/src/structures/shared/index.ts
@@ -0,0 +1,7 @@
+export * from './attachmentable';
+
+export * from './composer';
+
+export * from './message-forward';
+export * from './message-forwards-collection';
+export * from './message-reply';
