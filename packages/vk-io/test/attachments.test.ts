403e5d1189c82c8ddf1e158e5298f00439dcd72d
Negezor <negezor@gmail.com>
2020-07-29 01:19:36 +1100

    test: update tests for attachments

--- packages/vk-io/test/attachments.test.ts
+++ packages/vk-io/test/attachments.test.ts
@@ -1,4 +1,5 @@
 import {
+	VK,
 	Attachment,
 
 	WallAttachment,
@@ -11,36 +12,55 @@ import {
 	MarketAlbumAttachment
 } from '..';
 
+const vk = new VK({
+	token: process.env.TOKEN!
+});
+
 describe('Attachments', (): void => {
 	it('the main class must be equivalent to a string', (): void => {
-		const attachment = new Attachment('photo', 1234, 5678);
+		const attachment = new Attachment({
+			api: vk.api,
+			type: 'photo',
+			payload: {
+				id: 5678,
+				owner_id: 1234
+			}
+		});
 
 		expect(String(attachment)).toBe('photo1234_5678');
 	});
 
 	it('the main class must be equivalent to a string with access_key', (): void => {
-		const attachment = new Attachment('photo', 1234, 5678, 'ACCESS_KEY');
+		const attachment = new Attachment({
+			api: vk.api,
+			type: 'photo',
+			payload: {
+				id: 5678,
+				owner_id: 1234,
+				access_key: 'ACCESS_KEY'
+			}
+		});
 
 		expect(String(attachment)).toBe('photo1234_5678_ACCESS_KEY');
 	});
 
 	describe('the #fromString() should be correct working', (): void => {
 		it('should be throw an exception if wrong', (): void => {
-			expect((): Attachment => Attachment.fromString('ascbas_baasd')).toThrow();
-			expect((): Attachment => Attachment.fromString('12345/@%$%')).toThrow();
-			expect((): Attachment => Attachment.fromString('Incorrect')).toThrow();
-			expect((): Attachment => Attachment.fromString('1234_')).toThrow();
+			expect((): Attachment => Attachment.fromString('ascbas_baasd', vk.api)).toThrow();
+			expect((): Attachment => Attachment.fromString('12345/@%$%', vk.api)).toThrow();
+			expect((): Attachment => Attachment.fromString('Incorrect', vk.api)).toThrow();
+			expect((): Attachment => Attachment.fromString('1234_', vk.api)).toThrow();
 		});
 
 		it('should be correct parse', (): void => {
-			expect(Attachment.fromString('photo1234_5678')).toMatchObject({
+			expect(Attachment.fromString('photo1234_5678', vk.api)).toMatchObject({
 				type: 'photo',
 				ownerId: 1234,
 				id: 5678,
 				accessKey: undefined
 			});
 
-			expect(Attachment.fromString('photo1234_5678_ACCESS_KEY')).toMatchObject({
+			expect(Attachment.fromString('photo1234_5678_ACCESS_KEY', vk.api)).toMatchObject({
 				type: 'photo',
 				ownerId: 1234,
 				id: 5678,
@@ -51,7 +71,14 @@ describe('Attachments', (): void => {
 
 	describe('the #equals() should be correct working', (): void => {
 		it('should be throw an exception if wrong', (): void => {
-			const attachment = new Attachment('photo', 1234, 5678);
+			const attachment = new Attachment({
+				api: vk.api,
+				type: 'photo',
+				payload: {
+					id: 5678,
+					owner_id: 1234
+				}
+			});
 
 			expect((): boolean => attachment.equals('ascbas_baasd')).toThrow();
 			expect((): boolean => attachment.equals('inccorect')).toThrow();
@@ -59,27 +86,86 @@ describe('Attachments', (): void => {
 		});
 
 		it('should be return false', (): void => {
-			const attachment = new Attachment('photo', 1234, 5678);
+			const attachment = new Attachment({
+				api: vk.api,
+				type: 'photo',
+				payload: {
+					id: 5678,
+					owner_id: 1234
+				}
+			});
 
 			expect(attachment.equals('photo1234_1234')).toBe(false);
-			expect(attachment.equals(new Attachment('photo', 1234, 1234))).toBe(false);
-			expect(attachment.equals(new Attachment('photo', 1234, 1234, 'ACCESS_KEY'))).toBe(false);
+
+			expect(attachment.equals(
+				new Attachment({
+					api: vk.api,
+					type: 'photo',
+					payload: {
+						id: 1234,
+						owner_id: 1234
+					}
+				})
+			)).toBe(false);
+
+			expect(attachment.equals(
+				new Attachment({
+					api: vk.api,
+					type: 'photo',
+					payload: {
+						id: 1234,
+						owner_id: 1234,
+						access_key: 'ACCESS_KEY'
+					}
+				})
+			)).toBe(false);
 		});
 
 		it('should be return true', (): void => {
-			const attachment = new Attachment('photo', 1234, 5678);
+			const attachment = new Attachment({
+				api: vk.api,
+				type: 'photo',
+				payload: {
+					id: 5678,
+					owner_id: 1234
+				}
+			});
 
 			expect(attachment.equals('photo1234_5678')).toBe(true);
-			expect(attachment.equals(new Attachment('photo', 1234, 5678))).toBe(true);
-			expect(attachment.equals(new Attachment('photo', 1234, 5678, 'ACCESS_KEY'))).toBe(true);
+
+			expect(attachment.equals(
+				new Attachment({
+					api: vk.api,
+					type: 'photo',
+					payload: {
+						id: 5678,
+						owner_id: 1234
+					}
+				})
+			)).toBe(true);
+
+			expect(attachment.equals(
+				new Attachment({
+					api: vk.api,
+					type: 'photo',
+					payload: {
+						id: 5678,
+						owner_id: 1234,
+						access_key: 'ACCESS_KEY'
+					}
+				})
+			)).toBe(true);
 		});
 	});
 
 	describe('should equivalent to attaching to a string', (): void => {
 		it('wall', (): void => {
 			const attachment = new WallAttachment({
-				id: 4567,
-				owner_id: 1234
+				api: vk.api,
+				payload: {
+					id: 4567,
+					owner_id: 1234
+				}
 			});
 
 			expect(String(attachment)).toBe('wall1234_4567');
@@ -87,8 +173,11 @@ describe('Attachments', (): void => {
 
 		it('photo', (): void => {
 			const attachment = new PhotoAttachment({
-				id: 4567,
-				owner_id: 1234
+				api: vk.api,
+				payload: {
+					id: 4567,
+					owner_id: 1234
+				}
 			});
 
 			expect(String(attachment)).toBe('photo1234_4567');
@@ -96,8 +185,11 @@ describe('Attachments', (): void => {
 
 		it('audio', (): void => {
 			const attachment = new AudioAttachment({
-				id: 4567,
-				owner_id: 1234
+				api: vk.api,
+				payload: {
+					id: 4567,
+					owner_id: 1234
+				}
 			});
 
 			expect(String(attachment)).toBe('audio1234_4567');
@@ -105,8 +197,11 @@ describe('Attachments', (): void => {
 
 		it('story', (): void => {
 			const attachment = new StoryAttachment({
-				id: 4567,
-				owner_id: 1234
+				api: vk.api,
+				payload: {
+					id: 4567,
+					owner_id: 1234
+				}
 			});
 
 			expect(String(attachment)).toBe('story1234_4567');
@@ -114,8 +209,11 @@ describe('Attachments', (): void => {
 
 		it('video', (): void => {
 			const attachment = new VideoAttachment({
-				id: 4567,
-				owner_id: 1234
+				api: vk.api,
+				payload: {
+					id: 4567,
+					owner_id: 1234
+				}
 			});
 
 			expect(String(attachment)).toBe('video1234_4567');
@@ -123,8 +221,11 @@ describe('Attachments', (): void => {
 
 		it('market', (): void => {
 			const attachment = new MarketAttachment({
-				id: 4567,
-				owner_id: 1234
+				api: vk.api,
+				payload: {
+					id: 4567,
+					owner_id: 1234
+				}
 			});
 
 			expect(String(attachment)).toBe('market1234_4567');
@@ -132,8 +233,11 @@ describe('Attachments', (): void => {
 
 		it('document', (): void => {
 			const attachment = new DocumentAttachment({
-				id: 4567,
-				owner_id: 1234
+				api: vk.api,
+				payload: {
+					id: 4567,
+					owner_id: 1234
+				}
 			});
 
 			expect(String(attachment)).toBe('doc1234_4567');
@@ -141,8 +245,11 @@ describe('Attachments', (): void => {
 
 		it('market album', (): void => {
 			const attachment = new MarketAlbumAttachment({
-				id: 4567,
-				owner_id: 1234
+				api: vk.api,
+				payload: {
+					id: 4567,
+					owner_id: 1234
+				}
 			});
 
 			expect(String(attachment)).toBe('market_album1234_4567');
