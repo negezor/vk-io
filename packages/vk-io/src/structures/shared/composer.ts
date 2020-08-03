f8b18b8b69a45615cc7343b734ccb681c66474e7
Negezor <negezor@gmail.com>
2020-08-03 17:42:52 +1100

    refactor(shared): just export composer

--- packages/vk-io/src/structures/shared/composer.ts
+++ packages/vk-io/src/structures/shared/composer.ts
@@ -1,13 +1,1 @@
-import { Composer as MiddlewareComposer } from 'middleware-io';
-
-import { Context } from '../contexts';
-
-// @ts-expect-error
-export class Composer<T extends Context> extends MiddlewareComposer<T> {
-	/**
-	 * Create new `Composer` instance
-	 */
-	public static builder<T extends Context>(): Composer<T> {
-		return new Composer<T>();
-	}
-}
+export { Composer } from 'middleware-io';
