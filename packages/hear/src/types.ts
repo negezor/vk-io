f0b1f5f0cf4521e34a165f0e527118eb247a01f1
Negezor <negezor@gmail.com>
2020-08-19 17:31:16 +1100

    fix(hear): object properties should be partial

--- packages/hear/src/types.ts
+++ packages/hear/src/types.ts
@@ -5,8 +5,11 @@ export type HearFunctionCondition<T, V> = (value: V, context: T) => boolean;
 export type HearCondition<T, V> = HearFunctionCondition<T, V> | RegExp | string;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type HearObjectCondition<T extends Record<string, any>> = {
-	[P in keyof T]: AllowArray<HearCondition<T, T[P]>>;
+export type HearObjectCondition<T extends Record<string, any>> =
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+Record<string, AllowArray<HearCondition<T, any>>>
+& {
+	[P in keyof T]?: AllowArray<HearCondition<T, T[P]>>;
 };
 
 export type HearConditions<T> = (
