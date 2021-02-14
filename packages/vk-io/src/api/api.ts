ff497059cc5121f307605ab0bf5b00a3cb0d2213
Negezor <negezor@gmail.com>
2021-02-14 22:25:17 +1100

    chore: update api version for vk api

--- packages/vk-io/src/api/api.ts
+++ packages/vk-io/src/api/api.ts
@@ -203,7 +203,7 @@ class API {
 			apiRequestMode: 'sequential',
 			apiWait: 3e3,
 			apiLimit: 3,
-			apiVersion: '5.126',
+			apiVersion: '5.130',
 			apiBaseUrl: 'https://api.vk.com/method',
 			apiRetryLimit: 3,
 			apiTimeout: 10e3,
