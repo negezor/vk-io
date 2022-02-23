cf0163ba8045b17383acfaed6ef8bdafcc1ddd67
Negezor <negezor@gmail.com>
2022-02-23 16:42:19 +1100

    fix(api): skip execute in parallel selected

--- packages/vk-io/src/api/workers/parallel-selected.ts
+++ packages/vk-io/src/api/workers/parallel-selected.ts
@@ -2,6 +2,7 @@ import { ParallelWorker } from './parallel';
 
 export class ParallelSelectedWorker extends ParallelWorker {
 	protected skipMethod(method: string): boolean {
-		return !this.api.options.apiExecuteMethods.includes(method);
+		return super.skipMethod(method)
+			|| !this.api.options.apiExecuteMethods.includes(method);
 	}
 }
